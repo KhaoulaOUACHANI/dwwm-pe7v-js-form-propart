9d3e51142dc5a35effe9233dd9c88e98feeb571b
KhaoulaOUACHANI <k.ouachanii@gmail.com>
2023-08-04 12:44:43 +0200

    modif js

--- main.js
+++ main.js
@@ -1,9 +1,23 @@
-"use strict;"
+// Récupérer les éléments du DOM après chargement de la page
+document.addEventListener("DOMContentLoaded", function () {
+    const civilianForm = document.getElementById("civilian_form");
+    const profesionalForm = document.getElementById("profesional_form");
+    const civilianRadio = document.getElementById("civilian");
+    const profesionalRadio = document.getElementById("profesional");
 
-    function(propar)
-    {
-    if()
+    // Fonction pour cacher tous les formulaires sauf celui spécifié
+    function cacherFormulairesSauf(form) {
+        civilianForm.classList.add("disappear");
+        profesionalForm.classList.add("disappear");
+        form.classList.remove("disappear");
     }
 
+    // Écouter le changement du bouton radio pour afficher le formulaire correspondant
+    civilianRadio.addEventListener("change", function () {
+        cacherFormulairesSauf(civilianForm);
+    });
 
-window.addEventListener("load", function ());
+    profesionalRadio.addEventListener("change", function () {
+        cacherFormulairesSauf(profesionalForm);
+    });
+});
